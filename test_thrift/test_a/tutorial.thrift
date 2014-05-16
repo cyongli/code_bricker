2c0a6d95d3129fad55f94d5cd7718a20120c9919
chaoyli <lichaoyong121@qq.com>
2014-05-16 18:19:57 +0800

    modify cpp_knowledge

--- test_thrift/test_a/tutorial.thrift
+++ test_thrift/test_a/tutorial.thrift
@@ -1,21 +1,5 @@
 
-/**
- * The first thing to know about are types. The available types in Thrift are:
- *
- *  bool        Boolean, one byte
- *  byte        Signed byte
- *  i16         Signed 16-bit integer
- *  i32         Signed 32-bit integer
- *  i64         Signed 64-bit integer
- *  double      64-bit floating point value
- *  string      String
- *  binary      Blob (byte array)
- *  map<t1,t2>  Map from one type to another
- *  list<t1>    Ordered list of one type
- *  set<t1>     Set of unique elements of one type
- *
- * Did you also notice that Thrift supports C style comments?
- */
+include "shared.thrift"
 
 namespace cpp tutorial
 
@@ -44,9 +28,13 @@ exception InvalidOperation {
 }
 
 service Calculator extends shared.SharedService {
-   void ping(),
-   i32 add(1:i32 num1, 2:i32 num2),
-   i32 calculate(1:i32 logid, 2:Work w) throws (1:InvalidOperation ouch),
-   oneway void zip()
-}
 
+   void ping(),
+
+   i32 add(1:i32 num1, 2:i32 num2),
+
+   i32 calculate(1:i32 logid, 2:Work w) throws (1:InvalidOperation ouch),
+
+   oneway void zip()
+
+}
