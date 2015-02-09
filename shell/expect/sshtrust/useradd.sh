c16aafd7292ce859cd7d8df73f18357ac652ff69
cyongli <lichaoyong121@qq.com>
2015-02-09 16:27:32 +0800

    Update useradd.sh

--- shell/expect/sshtrust/useradd.sh
+++ shell/expect/sshtrust/useradd.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-passwd=$(perl -e 'print crypt($ARGV[0], "wtf")' "chaoyli")
+stty -echo
+echo -n "Enter password:" 
+read word 
+stty echo
+echo
+passwd=$(perl -e 'print crypt($ARGV[0], "wtf")' "$word")
 useradd -d /home/chaoyli -p $passwd chaoyli
