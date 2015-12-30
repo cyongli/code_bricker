c459de344ac7b1b69804a09138c873bf48ea6776
cyongli <lichaoyong121@qq.com>
2015-12-30 10:02:52 +0800

    add thread_specific.c

--- python/extract_by_regex/extract_time.py
+++ python/extract_by_regex/extract_time.py
@@ -5,7 +5,7 @@ f = open(sys.argv[1],"rw+")
 fw = open(sys.argv[2],"rw+")
 line = f.readline();
 
-k = re.compile("\[(time.*)\]");
+k = re.compile("\[(time[^\]]*)\]");
 k.search(line).group(1)
 while line:
     fw.write(k.search(line).group(1));
